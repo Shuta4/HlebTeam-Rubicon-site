c0ece708f4cacd15a2583ddd8d5a28b6b255a06e
SkirdinNikita <skirdin.nik@gmail.com>
2020-03-21 19:04:18 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.render("./pages/index");
-})
-app.get("/userpage", (req, res) => {
-  res.render("./pages/user_page")
-})
+  next();
+});
+app.get("/userpage", (req, res, next) => {
+  res.render("./pages/user_page");
+  next();
+});
 app.listen(port);
 console.log("Server are running on port: " + port);
