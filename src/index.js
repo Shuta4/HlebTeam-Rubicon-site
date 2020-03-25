53c66bb4fb62e9fec6b6c71b05befb88fd251f08
Nikita <skirdin.nik@gmail.com>
2020-03-25 11:06:22 +0300

    correct api route

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 
-app.use("/", require("./api.js"));
+app.use("/api", require("./api.js"));
 app.use("/", require("./pages.js"));
 
 app.listen(port);
