d8bcf27907620136580d67ccb2af23a797a2f628
Nikita <skirdin.nik@gmail.com>
2020-03-29 11:37:16 +0300

    update server db connecting

--- src/db/Tables/Works.js
+++ src/db/Tables/Works.js
@@ -1,16 +1,18 @@
 const sql = require('../connection.js');
 
 const Works = function() {
-	sql.connect();
-	sql.connection.query(`CREATE TABLE IF NOT EXISTS works (
+	connection = sql.connection();
+	sql.connect(connection);
+	connection.query(`CREATE TABLE IF NOT EXISTS works (
 			id int(12) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 			title varchar(100) NOT NULL,
 			created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			last_modified datetime
 		);`, (err, res) => {
-			if(err) console.log(err);
-	});
-	sql.end();
+			if(err) console.log("Error with creating table!\n" + err);
+			else console.log("Table 'works' was successfully created!");
+		});
+	sql.end(connection);
 }
 
 module.exports = Works;
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	  host: 'localhost',
-	  user: 'hlebteam',
-	  password: 'password',
-	  database: 'hlebteam'
-	});
-
 var sql = {
-	connection: connection,
-	connect: function() {
+	connection: function() {
+		mysql.createConnection({
+		  host: 'localhost',
+		  user: 'hlebteam',
+		  password: 'password',
+		  database: 'hlebteam'
+		});
+	},
+	connect: function(connection) {
 			connection.connect((err) => {
 			console.log("Error with connecting to db!");
 			console.log("ERROR: " + err);
 		})
 	},
-	end: function() {
+	end: function(connection) {
 			connection.end((err) => {
 			console.log("Error with ending connection to db!");
 			console.log("ERROR: " + err);
