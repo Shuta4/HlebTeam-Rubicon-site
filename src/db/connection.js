0722d2e4738b8386bcb7b415422e56ee65663b00
Nikita <skirdin.nik@gmail.com>
2020-03-29 11:22:05 +0300

    update rubicon 29.03.2020

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 var connection = mysql.createConnection({
 	  host: 'localhost',
-	  user: 'root',
-	  password: '',
-	  database: 'hlebteam-test'
+	  user: 'hlebteam',
+	  password: 'password',
+	  database: 'hlebteam'
 	});
 
 var sql = {
