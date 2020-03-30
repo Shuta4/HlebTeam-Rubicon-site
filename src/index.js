15dfff8ff30b30e6629b25bd3d028ff23d4fd737
Nikita <skirdin.nik@gmail.com>
2020-03-30 07:45:13 +0300

    Added cookie parser, session and users table

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const Works = require('./db/Tables/Works.js');
 const Images = require('./db/Tables/Images.js');
+const Users = require('./db/Tables/Users.js');
+const cookieParser = require('cookie-parser');
+const cookie = require('express-session')
 
 const port = 4000;
 const app = express();
 
-const sql = require('./db/connection')
+const sql = require('./db/connection');
 
+Users();
 Works();
 Images();
 
+app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
