d3e82b51c60dee4e2c54aff20a3da999fc9377ca
Nikita <skirdin.nik@gmail.com>
2020-04-05 08:34:13 +0300

    Fixed api

--- src/api.js
+++ src/api.js
@@ -7,50 +7,50 @@ router.get("/", (req, res, next) => {
   next();
 });
 
-router.router.post("/user/", (req, res, next)=> {
+router.post("/user/", (req, res, next)=> {
 	res.send("Создание пользователя");
 	next();
 });
-router.router.get("/user/find/:name", (req, res, next)=> {
+router.get("/user/find/:name", (req, res, next)=> {
 	res.send("Поиск пользователей по имени (дает краткую инфу (user-preview))");
 	next();
 });
-router.router.get("/user/:id", (req, res, next)=> {
+router.get("/user/:id", (req, res, next)=> {
 	res.send("Получение информации о 1 пользователе по id. Дает полную информацию (user-page)");
 	next();
 });
-router.router.put("/user/:id", (req, res, next)=> {
+router.put("/user/:id", (req, res, next)=> {
 	res.send("Изменение информации о пользователе (в req.body дается объект с информацией которую нужно изменить!)");
 	next();
 });
-router.router.delete("/user/:id", (req, res, next)=> {
+router.delete("/user/:id", (req, res, next)=> {
 	res.send("Удаление пользователя (только по id)");
 	next();
 });
 
-router.router.post("/work/", (req, res, next)=> {
+router.post("/work/", (req, res, next)=> {
 	res.send("Создание работы");
 	next();
 });
 //Возможно нужны альтернативные способы поиска работ
-router.router.get("/work/:id", (req, res, next)=> {
+router.get("/work/:id", (req, res, next)=> {
 	res.send("Получение информации о 1 работе по id. Дает полную информацию (work-page)");
 	next();
 });
-router.router.put("/work/:id", (req, res, next)=> {
+router.put("/work/:id", (req, res, next)=> {
 	res.send("Изменение информации о работе (в req.body дается объект с информацией которую нужно изменить!)");
 	next();
 });
-router.router.delete("/work/:id", (req, res, next)=> {
+router.delete("/work/:id", (req, res, next)=> {
 	res.send("Удаление работы (только по id)");
 	next();
 });
 
-router.router.post("/img/", (req, res, next)=> {
+router.post("/img/", (req, res, next)=> {
 	res.send("Загрузка фотографии на сервер");
 	next();
 });
-router.router.get("/img/:id", (req, res, next)=> {
+router.get("/img/:id", (req, res, next)=> {
 	res.send("Получение картинки"); // Возможно нужно больше способов получения
 	next();
 });
