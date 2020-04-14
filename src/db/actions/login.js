14f0aecf1bad33295861175591516fbe397a823c
Nikita <skirdin.nik@gmail.com>
2020-04-14 09:38:26 +0300

    fix

--- src/db/actions/login.js
+++ src/db/actions/login.js
@@ -1,6 +1,6 @@
 const sql = require("../connection.js");
 
-module.exports = function(username="", email="", password) {
+module.exports = function(username, email, password) {
 	if ((username || email) && password) {
 		user = getUserInfo(username, email);
 		if (!user.exist || !user.ok) return false;
