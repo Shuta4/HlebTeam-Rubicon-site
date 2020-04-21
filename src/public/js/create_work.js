059c686994c9bc393e8fdc86ff549f7764158cbc
Nikita <skirdin.nik@gmail.com>
2020-04-21 13:01:38 +0300

    update work form

--- src/public/js/create_work.js
+++ src/public/js/create_work.js
@@ -1,5 +1,5 @@
-import Swiper from "./swiper.min.js"
-var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100
-});
+// import Swiper from "./swiper.min.js"
+// var mySwiper = new Swiper('.swiper-container', {
+//     speed: 400,
+//     spaceBetween: 100
+// });
