94082e8d029966d9f398e0db4adb5ba7da25f1f4
Nikita <skirdin.nik@gmail.com>
2020-05-03 12:21:16 +0300

    fix

--- src/public/js/update_user.js
+++ src/public/js/update_user.js
@@ -1,8 +1,7 @@
 'use strict'
-var form = document.querySelector(".user-form__form");
-const messageBoxClass = ".message_box";
-
-document.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
+	var form = document.querySelector(".user-form__form");
+	const messageBoxClass = ".message_box";
 	fetch("/api/user/get/im").then((res)=> res.json()).then((res) => {
 		if (!res.ok) {
 			switch (res.error) {
@@ -24,51 +23,51 @@ document.onload = () => {
 		form.about.value = user.about;
 		form.birthday.value = birthday;
 	});
-}
-form.addEventListener("submit", (event) => {
-	event.preventDefault();
-	var user = {
-		username: form.username.value,
-		email: form.email.value,
-		name: form.name.value,
-		surname: form.surname.value,
-		about: form.about.value,
-		birthday: form.birthday.value,
-		old_password: form.old_password.value,
-		new_password: form.new_password.value,
-		confirm_password: form.confirm_password.value
-	}
-	if (user.new_password != user.confirm_password) {
-		form.querySelector(messageBoxClass).innerHTML = "Введенные пароли не совпадают!";
-		return;
-	}
-	fetch("/api/user/update/im", {
-	    method: 'PUT',
-	    headers: {
-	      'Content-Type': 'application/json'
-	    },
-	    body: JSON.stringify(user)
-  	}).then(res => res.json()).then(res => {
-		if (!res.ok) {
-			switch (res.error) {
-				case "ERRNOTLOGGEDIN":
-					form.querySelector(messageBoxClass).innerHTML = "Требуется войти в аккаунт";
-					window.location.href = "/userpage/im";
-					break
-				case "ERRDBCONNECTION":
-					form.querySelector(messageBoxClass).innerHTML = "Ошибка доступа к базе данных";
-					break
-				case "ERRINCORRECTPASSWORD":
-					form.querySelector(messageBoxClass).innerHTML = "Неправильный старый пароль";
-					break
-				case "ERRVALIDATEUSER":
-					form.querySelector(messageBoxClass).innerHTML = "Неправильный ввод";
-					break
-				default:
-					form.querySelector(messageBoxClass).innerHTML = "Неизвестная ошибка";
-			}
-			return
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		var user = {
+			username: form.username.value,
+			email: form.email.value,
+			name: form.name.value,
+			surname: form.surname.value,
+			about: form.about.value,
+			birthday: form.birthday.value,
+			old_password: form.old_password.value,
+			new_password: form.new_password.value,
+			confirm_password: form.confirm_password.value
 		}
-		window.location.href = "/userpage/im";
+		if (user.new_password != user.confirm_password) {
+			form.querySelector(messageBoxClass).innerHTML = "Введенные пароли не совпадают!";
+			return;
+		}
+		fetch("/api/user/update/im", {
+		    method: 'PUT',
+		    headers: {
+		      'Content-Type': 'application/json'
+		    },
+		    body: JSON.stringify(user)
+	  	}).then(res => res.json()).then(res => {
+			if (!res.ok) {
+				switch (res.error) {
+					case "ERRNOTLOGGEDIN":
+						form.querySelector(messageBoxClass).innerHTML = "Требуется войти в аккаунт";
+						window.location.href = "/userpage/im";
+						break
+					case "ERRDBCONNECTION":
+						form.querySelector(messageBoxClass).innerHTML = "Ошибка доступа к базе данных";
+						break
+					case "ERRINCORRECTPASSWORD":
+						form.querySelector(messageBoxClass).innerHTML = "Неправильный старый пароль";
+						break
+					case "ERRVALIDATEUSER":
+						form.querySelector(messageBoxClass).innerHTML = "Неправильный ввод";
+						break
+					default:
+						form.querySelector(messageBoxClass).innerHTML = "Неизвестная ошибка";
+				}
+				return
+			}
+			window.location.href = "/userpage/im";
+		});
 	});
-})
+});
